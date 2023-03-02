f5c5098577da432e50801e61126e5c8d82c5c592
Antoine <antoine_lanselle@msn.com>
2023-03-02 02:40:59 +0100

    writing the readme, fix integration tests and test report

--- README.md
+++ README.md
@@ -1,31 +1,30 @@
-# spring-boot
+# Poseidon
+
+Poseidon is a financial aggregator, powered by various tools on the market. 
+It is designed to help users manage their financial products more easily by bringing them together in one place.
+
+With PayMyBuddy, you will be able to:
+- Manage BidList, CurvePoint, Rating, RuleName and Trade.
+- Manage Users or Admins as Admin.
+- Log in with a Github account.
+- You can use Poseidon on your web browser or Postman.
+
 ## Technical:
 
-1. Framework: Spring Boot v2.0.4
+1. Spring Boot
 2. Java 8
 3. Thymeleaf
-4. Bootstrap v.4.3.1
 
+## Installation
 
-## Setup with Intellij IDE
-1. Create project from Initializr: File > New > project > Spring Initializr
-2. Add lib repository into pom.xml
-3. Add folders
-    - Source root: src/main/java
-    - View: src/main/resources
-    - Static: src/main/resource/static
-4. Create database with name "demo" as configuration in application.properties
-5. Run sql script to create table doc/data.sql
+# Database
 
-## Implement a Feature
-1. Create mapping domain class and place in package com.nnk.springboot.domain
-2. Create repository class and place in package com.nnk.springboot.repositories
-3. Create controller class and place in package com.nnk.springboot.controllers
-4. Create view files and place in src/main/resource/templates
+To create the database, open your SGBD and run the script doc/data.sql
 
-## Write Unit Test
-1. Create unit test and place in package com.nnk.springboot in folder test > java
+# Application
 
-## Security
-1. Create user service to load user from  database and place in package com.nnk.springboot.services
-2. Add configuration class and place in package com.nnk.springboot.config
+1. Clone the source repository using Git.
+2. Open your IDE, and run PayMyBuddyApplication.java to launch the application.
+3. To access the application you can open your browser, and go to http://localhost:8080 or use Postman
+4. Password to connect with an User is username:"user", password:"user"
+			    an Admin is username:"admin", password:"admin"
--- src/test/java/com/nnk/springboot/integration/UserIntegration.java
+++ src/test/java/com/nnk/springboot/integration/UserIntegration.java
@@ -1,15 +1,178 @@
 package com.nnk.springboot.integration;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @WithMockUser(username = "admin", roles = { "ADMIN" })
 public class UserIntegration {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	@BeforeEach
+	public void setUp() {
+		userRepository.deleteAll();
+
+		User user = new User();
+		user.setUsername("test");
+		user.setPassword("testPass%1");
+		user.setFullname("test");
+		user.setRole("USER");
+		userRepository.save(user);
+	}
+
+	@Test
+	public void validateUser_shouldAddUserInDatabase() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+
+		// WHEN
+		mockMvc.perform(post("/user/validate").param("username", "newTest").param("password", "newTestPass%1")
+				.param("fullname", "newTest").param("role", "USER")).andExpect(status().is3xxRedirection());
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals(2, userRepository.findAll().size());
+		assertEquals("newTest", userRepository.findAll().get(1).getUsername());
+	}
+
+	@Test
+	public void updateUser_shouldUpdateUserInDatabase() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+
+		// WHEN
+		mockMvc.perform(post("/user/update/{id}", listBefore.get(0).getId()).param("username", "newTest")
+				.param("password", "newTestPass%1").param("fullname", "newTest").param("role", "USER"))
+				.andExpect(status().is3xxRedirection());
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals("newTest", userRepository.findById(listBefore.get(0).getId()).get().getUsername());
+		assertEquals(1, userRepository.findAll().size());
+	}
+
+	@Test
+	public void deleteUser_shouldDeleteUserInDatabase() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+
+		// WHEN
+		mockMvc.perform(get("/user/delete/{id}", listBefore.get(0).getId())).andExpect(status().is3xxRedirection());
+		// the id changes at each test so we don't put a constant variable
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals(0, userRepository.findAll().size());
+	}
+
+	@Test
+	public void getUser_shouldReturnListOfUser() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+
+		// WHEN
+		mockMvc.perform(get("/user/all"));
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals(1, userRepository.findAll().size());
+	}
+
+	@Test
+	public void addUser_shouldAddUserInRepository() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+
+		User user = new User();
+		user.setUsername("newTest");
+		user.setPassword("newTestPass%1");
+		user.setFullname("newTest");
+		user.setRole("USER");
+
+		MediaType MEDIA_TYPE_JSON = new MediaType("application", "json");
+		MockHttpServletRequestBuilder request = post("/user");
+		request.content(objectMapper.writeValueAsString(user));
+		request.accept(MEDIA_TYPE_JSON);
+		request.contentType(MEDIA_TYPE_JSON);
+
+		// WHEN
+		mockMvc.perform(request).andExpect(status().isOk());
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals(2, userRepository.findAll().size());
+		assertEquals("newTest", userRepository.findAll().get(1).getUsername());
+	}
+
+	@Test
+	public void updateUser_shouldUpdateTheUserInDatabase() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+		
+		User user = new User();
+		user.setId(listBefore.get(0).getId());
+		user.setUsername("newTest");
+		user.setPassword("newTestPass%1");
+		user.setFullname("newTest");
+		user.setRole("USER");
+
+		MediaType MEDIA_TYPE_JSON = new MediaType("application", "json");
+		MockHttpServletRequestBuilder request = put("/user");
+		request.content(objectMapper.writeValueAsString(user));
+		request.accept(MEDIA_TYPE_JSON);
+		request.contentType(MEDIA_TYPE_JSON);
+
+		// WHEN
+		mockMvc.perform(request).andExpect(status().isOk());
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals("newTest", userRepository.findAll().get(0).getUsername());
+		assertEquals(1, userRepository.findAll().size());
+	}
+
+	@Test
+	public void deleteUser_shouldDeleteTheUserInDatabase() throws Exception {
+		// GIVEN
+		List<User> listBefore = userRepository.findAll();
+
+		// WHEN
+		mockMvc.perform(delete("/user").param("id", listBefore.get(0).getId().toString())).andExpect(status().isOk());
+		// the id changes at each test so we don't put a constant variable
+
+		// THEN
+		assertEquals(1, listBefore.size());
+		assertEquals(0, userRepository.findAll().size());
+	}
 
 }
